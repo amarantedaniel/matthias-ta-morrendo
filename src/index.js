1c377175d79f31c174423a52deaf294d8d00c2fd
Daniel Amarante <daniel.amarante2@gmail.com>
2019-04-27 12:32:42 -0300

    Fix absolute paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './scenes/Home'
+import Home from 'scenes/Home'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<Home />, document.getElementById('root'))
