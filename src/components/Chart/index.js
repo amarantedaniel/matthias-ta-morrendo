a8651ad745439dfb1cd8437cb7503c96fbedd780
Daniel Amarante <daniel.amarante2@gmail.com>
2019-04-27 22:05:03 -0300

    Make chart responsive

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts'
+import {
+  LineChart,
+  ResponsiveContainer,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from 'recharts'
 
 const Chart = ({ data }) => (
-  <LineChart width={1500} height={300} data={data}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="date" />
-    <YAxis />
-    <Line type="monotone" dataKey="glicose" stroke="#8884d8" />
-  </LineChart>
+  <ResponsiveContainer width="95%" height={300}>
+    <LineChart data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Line type="monotone" dataKey="glicose" stroke="#8884d8" />
+    </LineChart>
+  </ResponsiveContainer>
 )
 
 export default Chart
